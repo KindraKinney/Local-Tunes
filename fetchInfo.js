3b81f502f06628bcd24628e3d2646a7cc539b50f
David <david@thejurne.com>
2020-01-18 11:29:00 -0800

    add api url

--- fetchInfo.js
+++ fetchInfo.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   const str;
   $('#submit').on('click', () => {
     str = $('#user-input').val();
+
+    const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=';
   });
-
-
 });
