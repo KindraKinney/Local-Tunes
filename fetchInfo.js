814d519df68b266c5e085c83b8f6f6f10f5d74f9
David <david@thejurne.com>
2020-01-18 11:29:48 -0800

    undo url

--- fetchInfo.js
+++ fetchInfo.js
@@ -2,7 +2,5 @@ $(document).ready(() => {
   const str;
   $('#submit').on('click', () => {
     str = $('#user-input').val();
-
-    const url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=';
   });
 });
