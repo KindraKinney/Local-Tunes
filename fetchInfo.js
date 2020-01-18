aa2d074836d980c24057b75789c5537f77723264
James Dobbe <dobbe2@gmail.com>
2020-01-18 13:18:08 -0800

    wrote the JQUERY code to grab the info (currently artist and song name) and append them to the #output-box in the HTML

--- fetchInfo.js
+++ fetchInfo.js
@@ -2,5 +2,33 @@ $(document).ready(() => {
   const str;
   $('#submit').on('click', () => {
     str = $('#user-input').val();
+
+  
   });
+
+
 });
+ 
+ 
+
+  //JQUERY FUNCTION FOR SETTING SONGNAME AND ARTIST NAME TO OUTPUTBOX DIV//
+//   function outputBox(){
+
+// let songName = "mmmBop";
+// let artistName = "Hansen";
+// let mainInfo = $("<div class='output-box'>");
+// let songDiv = $("<div class='song'>");
+// $(songDiv).addClass("mdc-typography--headline6")
+// let artistDiv = $("<div class='artist'>");
+// $(artistDiv).addClass("mdc-typography--subtitle1");
+// let pOne = $("<p>").text("Song Title: " + songName);
+// songDiv.append(pOne);
+// mainInfo.append(songDiv);
+// let pTwo = $("<p>").text("Artist Name: " + artistName);
+// artistDiv.append(pTwo);
+// mainInfo.append(artistDiv);
+// $("#output-box").prepend(mainInfo);
+// }
+  
+
+// outputBox();
