dfd59f25ac692ab452d9e3af33a22dd5cf00bfaf
David <david@thejurne.com>
2020-01-22 21:14:01 -0800

    nothing

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ $(document).ready(() => {
     $('#submit').on('click', () => {
       debugger;
 
-      let countryCode = $("#user-input").val();
-      let makeRequest = $("<script>").attr("src", `https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=response&country=${countryCode}&apikey=744d96e601e068c973cbbc1a33372ce4`)
-      $("body").append(makeRequest);
+      let countryCode = $('#user-input').val();
+      let makeRequest = $('<script>').attr('src', `https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=response&country=${countryCode}&apikey=744d96e601e068c973cbbc1a33372ce4`)
+      $('body').append(makeRequest);
     });
   });
 
@@ -33,29 +33,29 @@ $(document).ready(() => {
                     let currentTrack = response.tracks.track[i].name;
                     let currentListen = response.tracks.track[i].listeners;
                     let currentArtist = response.tracks.track[i].artist.name;
-                    let topPicks = `<div class="mdc-typography--headline6">Title: ${currentTrack}</div>
-                                    <div class="mdc-typography--subtitle1">Artist: ${currentArtist}</div>
-                                    <div class="mdc-typography--subtitle1">Listen Count: ${currentListen}</div>
+                    let topPicks = `<div class='mdc-typography--headline6'>Title: ${currentTrack}</div>
+                                    <div class='mdc-typography--subtitle1'>Artist: ${currentArtist}</div>
+                                    <div class='mdc-typography--subtitle1'>Listen Count: ${currentListen}</div>
                                     <br>`
-                                    $("#output-box").append(topPicks);
+                                    $('#output-box').append(topPicks);
                 }
-                $("#output-box").prepend("<div class='mdc-typography--headline4'>Top Tracks</div>", "<br>");
-              
+                $('#output-box').prepend('<div class='mdc-typography--headline4'>Top Tracks</div>', '<br>');
+
           }
       });
     });
-  
+
   window.onload = function() {
     var startPos;
     var geoSuccess = function(position) {
       startPos = position;
       latlng = startPos.coords.latitude + ',' + startPos.coords.longitude;
-      APIkey = "AIzaSyDrCceye243-Te4hjHSsc7h3LcwzY-1xeI";
-      queryURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlng + "&key=" + APIkey;
+      APIkey = 'AIzaSyDrCceye243-Te4hjHSsc7h3LcwzY-1xeI';
+      queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latlng + '&key=' + APIkey;
 
       $.ajax({
         url: queryURL,
-        method: "GET"
+        method: 'GET'
       }).then(response => {
         console.log(response.results[7].address_components[0].long_name);
       });
