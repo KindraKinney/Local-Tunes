6eecf4bbf9c118309679962e05e9e8d4e8bd8b55
David <david@thejurne.com>
2020-01-25 10:14:14 -0800

    fix paren bug

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-
-
 $(document).ready(() => {
-  $('#submit').on('click', () => {
-    const countryCode = $('#user-input').val();
-    const makeRequest = $('<script>').attr('src', `https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=response&country=${countryCode}&apikey=744d96e601e068c973cbbc1a33372ce4`)
-    $('body').append(makeRequest);
-  });
+  const inputPosition = () => {
+    let startPos;
+    const geoSuccess = position => {
+      startPos = position;
+      const latlng = startPos.coords.latitude + ',' + startPos.coords.longitude;
+      const APIkey = 'AIzaSyDrCceye243-Te4hjHSsc7h3LcwzY-1xeI';
+      const queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latlng + '&key=' + APIkey;
+
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      }).then(response => {
+        $("#output-box").append(response.results[10].address_components[0].long_name);
+      });
+      navigator.geolocation.getCurrentPosition(geoSuccess);
+    };
+  }
 
   const response = musicData => {
     $('output-box').empty();
@@ -22,23 +32,11 @@ $(document).ready(() => {
     }
   }
 
+  $('#submit').on('click', () => {
+    const countryCode = $('#user-input').val();
+    const makeRequest = $('<script>').attr('src', `https://api.musixmatch.com/ws/1.1/chart.tracks.get?format=jsonp&callback=response&country=${countryCode}&apikey=744d96e601e068c973cbbc1a33372ce4`);
+    $('body').append(makeRequest);
+  });
+
   inputPosition();
-
-  const inputPosition = () => {
-    let startPos;
-    const geoSuccess = position => {
-      startPos = position;
-      const latlng = startPos.coords.latitude + ',' + startPos.coords.longitude;
-      const APIkey = 'AIzaSyDrCceye243-Te4hjHSsc7h3LcwzY-1xeI';
-      const queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latlng + '&key=' + APIkey;
-
-      $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(response => {
-        console.log(response.results[10].address_components[0].long_name);
-        $("#output-box").append(response.results[10].address_components[0].long_name);
-      });
-      navigator.geolocation.getCurrentPosition(geoSuccess);
-    };
-};
+});
